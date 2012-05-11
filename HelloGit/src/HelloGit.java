4c920a9296cfc982e85293c68952ee2930bdd881
Ryo <ma11044@shibaura-it.ac.jp>
2012-05-11 20:41:57 +0900

    test

---
+++ HelloGit/src/HelloGit.java
@@ -0,0 +1,13 @@
+
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("Hello Git.");
+		System.out.println("HELLO");
+	}
+
+}
